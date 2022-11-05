18ff477a42791565e6416706d23a479445999aeb
gopps95 <shankargopi7@gmail.com>
2022-11-05 21:31:24 +0530

    botlive

--- bot.py
+++ bot.py
@@ -1,4 +1,13 @@
 import hikari
 
 bot = hikari.GatewayBot(token='MTAzODQ2MTkzNzIwMzMwMjUwMg.GZ4ryE.LkrdHAoIMSY4-8bb_DNdfWHdLHVtPSO_T23uQE')
+
+@bot.listen(hikari.GuildMessageCreateEvent)
+async def print_message(event):
+    print(event.content)
+
+@bot.listen(hikari.StartedEvent)
+async def bot_started(event):
+    print("Hey dude")
+
 bot.run()
