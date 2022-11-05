1797dcbd578e176b4c942cd6f413e79a72d8c10d
gopps95 <shankargopi7@gmail.com>
2022-11-05 22:25:10 +0530

    discord1

--- bot.py
+++ bot.py
@@ -1,7 +1,18 @@
+import hikari
 import lightbulb
 
-bot = lightbulb.BotApp(token='MTAzODQ2MTkzNzIwMzMwMjUwMg.GZ4ryE.LkrdHAoIMSY4-8bb_DNdfWHdLHVtPSO_T23uQE')
+bot = lightbulb.BotApp(
+ token='MTAzODQ2MTkzNzIwMzMwMjUwMg.GZ4ryE.LkrdHAoIMSY4-8bb_DNdfWHdLHVtPSO_T23uQE',
+ default_enabled_guilds=(1038460338020036668)
+)
 
+@bot.listen(hikari.StartedEvent)
+async def on_started(event):
+    print("Bot")
 
-
+@bot.command
+@lightbulb.command('ping','pong')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond("Pong!")
 bot.run()
