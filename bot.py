5c30d41a9723bbebbdb2a7654b04a67e194c1dd2
gopps95 <shankargopi7@gmail.com>
2022-11-05 21:39:40 +0530

    lightbulb

--- bot.py
+++ bot.py
@@ -1,13 +1,7 @@
-import hikari
+import lightbulb
 
-bot = hikari.GatewayBot(token='MTAzODQ2MTkzNzIwMzMwMjUwMg.GZ4ryE.LkrdHAoIMSY4-8bb_DNdfWHdLHVtPSO_T23uQE')
+bot = lightbulb.BotApp(token='MTAzODQ2MTkzNzIwMzMwMjUwMg.GZ4ryE.LkrdHAoIMSY4-8bb_DNdfWHdLHVtPSO_T23uQE')
 
-@bot.listen(hikari.GuildMessageCreateEvent)
-async def print_message(event):
-    print(event.content)
 
-@bot.listen(hikari.StartedEvent)
-async def bot_started(event):
-    print("Hey dude")
 
 bot.run()
